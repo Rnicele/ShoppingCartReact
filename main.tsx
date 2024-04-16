bf7dc0df608fed2d2edeb1d05cc6f584d886d3ea
Nicele Reyes <nicele.reyes26@gmail.com>
2024-04-16 18:09:39 +0800

    move main.tsx to root directory

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./src/App.tsx";
 import "./index.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
