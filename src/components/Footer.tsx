8ff8d35046a5fe3249ecf5eac6f4bfedf49c42c1
Nicele Reyes <nicele.reyes26@gmail.com>
2024-04-16 21:22:39 +0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ const Footer = () => {
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <a href="/shopping-cart-react" className="nav-link px-2 text-muted">
+            <a
+              href="/shopping-cart-react/"
+              className="nav-link px-2 text-muted"
+            >
               Home
             </a>
           </li>
