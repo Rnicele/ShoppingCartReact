5333700b7914128434605077362a94dc92e5c1de
Nicele Reyes <nicele.reyes26@gmail.com>
2024-04-16 18:48:28 +0800

    run build

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./src/App";
-import "./index.css";
-import "./App.css";
+import "./src/index.css";
+import "./src/App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
